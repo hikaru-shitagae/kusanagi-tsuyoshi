0bf5c7e821ac5780c032f232fb9c7f9fe30bb529
hikaru-shitagae <hikaru1628umada@gmail.com>
2024-06-16 22:33:17 +0900

    こんな感じでデザインしました

--- osake.py
+++ osake.py
@@ -6,7 +6,7 @@ import json
 app = Flask(__name__)
 app.json.ensure_ascii = False
 
-@app.route('/index.html')
+@app.route('/')
 def index():
     con = sqlite3.connect('sake_database.db')
     cur = con.cursor()
@@ -14,17 +14,17 @@ def index():
     alco_array = []
 
     for row in cur.execute("select * from liquor;"):
-        alco_array.append([row[0],row[1]])
+        alco_array.append([row[0], row[1]])
 
     con.close()
 
     return render_template('index.html', data=alco_array)
 
-@app.route('/alcohol.html')
+@app.route('/alcohol')
 def alcohol():
     return render_template('alcohol.html')
 
-@app.route('/shop.html')
+@app.route('/shop')
 def shop():
     return render_template('shop.html')
 
@@ -38,12 +38,12 @@ def answershop():
     capacity = request.form['capacity']
     mood = request.form['mood']
     s = '<!DOCTYPE html>\n'
-    s += '<html><head><meta charset=UTF-8">\n'
+    s += '<html><head><meta charset="UTF-8">\n'
     s += '<link rel="stylesheet" href="static/style_alcohol.css">\n'
-    s += '<script scr="static/osake.js"></script></head>\n'
+    s += '<script src="static/osake.js"></script></head>\n'
     s += '<body><h2>以下のような居酒屋がおすすめです。</h2>\n'
     i = 0
-    for row in cur.execute("select * from shop where address like '%{}%' and price like '%{}%' and capacity like '%{}%' and mood like '%{}&';".format(address, price, capacity, mood)):
+    for row in cur.execute("select * from shop where address like '%{}%' and price like '%{}%' and capacity like '%{}%' and mood like '%{}%';".format(address, price, capacity, mood)):
         s += '<section class="sake">'
         s += '<div class="btn-container">\n'
         s += '<p>{}</p>\n'.format(row[0])
@@ -56,13 +56,11 @@ def answershop():
         s += '<li>味：{}</li>\n'.format(row[3])
         s += '<li>割り材：{}</li>\n'.format(row[4])
         s += '<li>こんな人におすすめ：{}</li>\n'.format(row[5])
-        s += '<li>レビュー：{}</li>\n'.format(row[6])
-        s += '</ul></section>\n'
         s += '<a href="https://www.google.co.jp/maps/place/{}">お店の場所はこちら</a><br>'.format(address)
         s += '</section>\n'
-    s += '<p>検索結果：{}件</p>\n'.format(i)
-    s += '<a href="index.html">トップに戻る</a>\n'
-    s += '</body></html>'
+        s += '<p>検索結果：{}件</p>\n'.format(i)
+        s += '<a href="/">トップに戻る</a>\n'
+        s += '</body></html>\n'
     con.close()
     return s
 
@@ -98,12 +96,11 @@ def answeralco():
         s += '</ul></section><br>\n'
         s += '<center><a href="https://www.amazon.co.jp/s?k={}">購入はこちらから</a></center><br>'.format(row[0])
         s += '</section>\n'
-    s += '<p>検索結果：{}件</p>\n'.format(i)
-    s += '<a href="index.html">トップに戻る</a>\n'
-    s +=  '</body></html>'
+        s += '<p>検索結果：{}件</p>\n'.format(i)
+        s += '<a href="/">トップに戻る</a>\n'
+        s += '</body></html>\n'
     con.close()
     return s
-
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
